3bc64fb456ae2bf74b39f96b9fe2076c6841e37f
rahuldhayal67-cell <rahuldhayal67@gmail.com>
2026-09-26 09:12:32 +0530

    Add Rajasthan 2026 town/city dataset
    This file contains a dataset of towns and cities in Rajasthan for the year 2026, structured in a JavaScript object format.

---
+++ rajasthan-cities-2026.js
@@ -0,0 +1,465 @@
+// Connect — Rajasthan 2026 town/city dataset
+
+const rajasthanCities = {
+  "Ajmer": [
+    "Ajmer",
+    "Kekri",
+    "Kishangarh",
+    "Nasirabad",
+    "Pisangan",
+    "Pushkar",
+    "Sarwar",
+    "Sawar",
+    "Tantoti"
+  ],
+
+  "Alwar": [
+    "Alwar",
+    "Bahadurpur",
+    "Baroda Mev",
+    "Bardod",
+    "Govindgarh",
+    "Kathumar",
+    "Khairthal",
+    "Kherli",
+    "Kotkasim",
+    "Laxmangarh",
+    "Malakhera",
+    "Mubarikpur",
+    "Mundawar",
+    "Naugawa",
+    "Neemrana",
+    "Reni",
+    "Rajgarh",
+    "Ramgarh",
+    "Tapukada",
+    "Thanagazi",
+    "Tijara"
+  ],
+
+  "Balotra": [
+    "Balotra",
+    "Samdari",
+    "Siwana",
+    "Sindhari"
+  ],
+
+  "Banswara": [
+    "Banswara",
+    "Ghatol",
+    "Kushalgarh",
+    "Partapur-Garhi"
+  ],
+
+  "Baran": [
+    "Antah",
+    "Atru",
+    "Baran",
+    "Chhabra",
+    "Mangrol",
+    "Siswali"
+  ],
+
+  "Barmer": [
+    "Barmer",
+    "Chohtan",
+    "Dhorimanna",
+    "Gudamalani",
+    "Jasol"
+  ],
+
+  "Beawar": [
+    "Beawar",
+    "Jaitaran",
+    "Masuda",
+    "Raipur",
+    "Vijainagar"
+  ],
+
+  "Bharatpur": [
+    "Bharatpur",
+    "Bayana",
+    "Bhusawar",
+    "Nadbai",
+    "Roopwas",
+    "Uchain",
+    "Weir"
+  ],
+
+  "Bhilwara": [
+    "Asind",
+    "Banera",
+    "Bhilwara",
+    "Bigod",
+    "Bijolia",
+    "Gangapur",
+    "Gulabpura",
+    "Hameergarh",
+    "Jahazpur",
+    "Mandalgarh",
+    "Raipur",
+    "Sahara",
+    "Shahpura"
+  ],
+
+  "Bikaner": [
+    "Bikaner",
+    "Deshnoke",
+    "Dungargarh",
+    "Khajuwala",
+    "Lunkaransar",
+    "Napasar",
+    "Nokha"
+  ],
+
+  "Bundi": [
+    "Bundi",
+    "Dei",
+    "Hindoli",
+    "Indragarh",
+    "Kaprain",
+    "Keshoraipatan",
+    "Lakheri",
+    "Nainwa"
+  ],
+
+  "Chittorgarh": [
+    "Chittorgarh",
+    "Bari Sadri",
+    "Begun",
+    "Kapasan",
+    "Nimbahera",
+    "Rawatbhata"
+  ],
+
+  "Churu": [
+    "Bidasar",
+    "Chhapar",
+    "Churu",
+    "Rajgarh",
+    "Rajaldesar",
+    "Ratangarh",
+    "Ratannagar",
+    "Sahwa",
+    "Sardarshahar",
+    "Sujangarh",
+    "Taranagar"
+  ],
+
+  "Dausa": [
+    "Bandikui",
+    "Baswa",
+    "Bhandarej",
+    "Dausa",
+    "Lalsot",
+    "Lawan",
+    "Mahwa",
+    "Mandawari",
+    "Mandawar",
+    "Ramgarh Pachwara",
+    "Sikrai"
+  ],
+
+  "Deeg": [
+    "Deeg",
+    "Kaman",
+    "Kumher",
+    "Nagar",
+    "Pahari",
+    "Sikri"
+  ],
+
+  "Dholpur": [
+    "Bari",
+    "Baseri",
+    "Dholpur",
+    "Mania",
+    "Rajakhera",
+    "Sarmathura",
+    "Sepau"
+  ],
+
+  "Didwana-Kuchaman": [
+    "Borawar",
+    "Degana",
+    "Didwana",
+    "Kuchaman City",
+    "Kuchera",
+    "Khatu Khurd",
+    "Ladnu",
+    "Makrana",
+    "Merta Road",
+    "Nawa",
+    "Parbatsar",
+    "Riyanbari"
+  ],
+
+  "Dungarpur": [
+    "Aspur",
+    "Dungarpur",
+    "Sagwara",
+    "Simalwara"
+  ],
+
+  "Hanumangarh": [
+    "Bhadra",
+    "Goluwala",
+    "Hanumangarh",
+    "Nohar",
+    "Pilibanga",
+    "Rawatsar",
+    "Sangaria",
+    "Tibbi"
+  ],
+
+  "Jaipur": [
+    "Bagru",
+    "Bassi",
+    "Chaksu",
+    "Chomu",
+    "Dudu",
+    "Jaipur",
+    "Jamwa Ramgarh",
+    "Jobner",
+    "Kaladera",
+    "Kanota",
+    "Khejroli",
+    "Kishangarh Renwal",
+    "Manoharpur",
+    "Naraina",
+    "Phagi",
+    "Phulera",
+    "Sambhar",
+    "Shahpura",
+    "Vatika"
+  ],
+
+  "Jaisalmer": [
+    "Jaisalmer",
+    "Pokaran"
+  ],
+
+  "Jalore": [
+    "Ahore",
+    "Bhinmal",
+    "Jalore",
+    "Raniwara",
+    "Sanchore",
+    "Sayla"
+  ],
+
+  "Jhalawar": [
+    "Aklera",
+    "Bhawani Mandi",
+    "Dag",
+    "Jhalawar",
+    "Jhalrapatan",
+    "Khanpur",
+    "Manohar Thana",
+    "Pirawa"
+  ],
+
+  "Jhunjhunu": [
+    "Baggar",
+    "Bissau",
+    "Buhana",
+    "Chirawa",
+    "Dundlod",
+    "Gudha Gorji",
+    "Jakhal",
+    "Jhunjhunu",
+    "Khetri",
+    "Malsisar",
+    "Mandawa",
+    "Mandrella",
+    "Mukundgarh",
+    "Nawalgarh",
+    "Pilani",
+    "Singhana",
+    "Sultana",
+    "Surajgarh",
+    "Udaipurwati",
+    "Vidyavihar"
+  ],
+
+  "Jodhpur": [
+    "Balesar",
+    "Bap",
+    "Bilara",
+    "Bhopalgarh",
+    "Jodhpur",
+    "Mathania",
+    "Pipar City",
+    "Tinwari"
+  ],
+
+  "Karauli": [
+    "Hindaun",
+    "Karauli",
+    "Mandrail",
+    "Mandrayal",
+    "Sapotra",
+    "Todabhim"
+  ],
+
+  "Khairthal-Tijara": [
+    "Bhiwadi",
+    "Khairthal",
+    "Kishangarhbas",
+    "Kotkasim",
+    "Mundawar",
+    "Tijara"
+  ],
+
+  "Kota": [
+    "Itawa",
+    "Kaithoon",
+    "Kota",
+    "Ramganj Mandi",
+    "Sangod",
+    "Suket"
+  ],
+
+  "Kotputli-Behror": [
+    "Bansur",
+    "Behror",
+    "Kotputli",
+    "Mandhan",
+    "Narayanpur",
+    "Paota",
+    "Viratnagar"
+  ],
+
+  "Nagaur": [
+    "Degana",
+    "Jayal",
+    "Kuchera",
+    "Merta City",
+    "Mundwa",
+    "Nagaur"
+  ],
+
+  "Pali": [
+    "Bali",
+    "Falna",
+    "Jaitaran",
+    "Marwar Junction",
+    "Pali",
+    "Rani",
+    "Sadri",
+    "Sojat",
+    "Sojat Road",
+    "Sumerpur",
+    "Takhatgarh"
+  ],
+
+  "Phalodi": [
+    "Bap",
+    "Phalodi"
+  ],
+
+  "Pratapgarh": [
+    "Arnod",
+    "Chhoti Sadri",
+    "Dhariawad",
+    "Pratapgarh"
+  ],
+
+  "Rajsamand": [
+    "Amet",
+    "Bhim",
+    "Deogarh",
+    "Nathdwara",
+    "Rajsamand"
+  ],
+
+  "Salumber": [
+    "Chawand",
+    "Salumber",
+    "Semari"
+  ],
+
+  "Sawai Madhopur": [
+    "Bamanwas",
+    "Bonli",
+    "Gangapur City",
+    "Khandar",
+    "Khirni",
+    "Sawai Madhopur",
+    "Wazirpur"
+  ],
+
+  "Sikar": [
+    "Danta",
+    "Dhod",
+    "Fatehpur",
+    "Khandela",
+    "Khatushyam Ji",
+    "Lachhmangarh",
+    "Losal",
+    "Neem-Ka-Thana",
+    "Palsana",
+    "Ramgarh Shekhawati",
+    "Reengus",
+    "Sikar",
+    "Sri Madhopur"
+  ],
+
+  "Sirohi": [
+    "Abu Road",
+    "Jawal",
+    "Mandar",
+    "Mount Abu",
+    "Pindwara",
+    "Sheoganj",
+    "Sirohi"
+  ],
+
+  "Sri Ganganagar": [
+    "Anupgarh",
+    "Gajsinghpur",
+    "Gharsana",
+    "Karanpur",
+    "Kesrisinghpur",
+    "Padampur",
+    "Raisinghnagar",
+    "Sadulshahar",
+    "Sri Ganganagar",
+    "Suratgarh",
+    "Vijainagar"
+  ],
+
+  "Tonk": [
+    "Deoli",
+    "Diggi",
+    "Dooni",
+    "Lamba Hari Singh",
+    "Malpura",
+    "Niwai",
+    "Peeplu",
+    "Todaraisingh",
+    "Tonk",
+    "Uniara"
+  ],
+
+  "Udaipur": [
+    "Bhinder",
+    "Fatehnagar",
+    "Kanor",
+    "Rishabhdeo",
+    "Salumber",
+    "Udaipur",
+    "Vallabhnagar"
+  ]
+};
+
+
+// Convert data to the format used by Connect
+cityData = Object.entries(rajasthanCities).flatMap(
+  ([district, cities]) =>
+    cities.map(city => ({
+      state: "Rajasthan",
+      district: district,
+      city: city
+    }))
+);
